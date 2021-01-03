59c767ea6e640587b8c4835afffeee4e323dc469
George <george.gbenle@outlook.com>
2021-01-03 17:10:35 -0500

    Create my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -6,4 +6,20 @@ module Enumerable
     end
 end
 
-    
+
+module Enumerable
+    def my_each_with_index
+        arr = to_a
+        arr.length.times do |item|
+            yield arr[item], item
+        end 
+    end
+end
+
+# my_array = [2, 3, 4, 5, 99, 7, 78, 9, 10]
+
+# my_array.my_each_with_index do |item, i|
+#     if i.even?
+#         puts item * 2
+# end
+# end
