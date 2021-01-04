e795220957985352909751e12a22b8fd7aa3dcbf
George <george.gbenle@outlook.com>
2021-01-04 08:33:19 -0500

    Create my_none method

--- enumerables.rb
+++ enumerables.rb
@@ -1,7 +1,7 @@
 module Enumerable
     def my_each(arr = to_a)
-            arr.length.times do |item|
-                yield arr[item]
+            arr.length.times do |object|
+                yield arr[object]
             end
     end
 end
@@ -10,8 +10,8 @@ end
 module Enumerable
     def my_each_with_index
         arr = to_a
-        arr.length.times do |item|
-            yield arr[item], item
+        arr.length.times do |object|
+            yield arr[object], object
         end 
     end
 end
@@ -19,8 +19,8 @@ end
 module Enumerable
     def my_select
         array = []
-        my_each do |item|
-            array.push(item) if yield(item)
+        my_each do |object|
+            array.push(object) if yield(object)
             end
         array
     end
@@ -28,9 +28,8 @@ end
 
 module Enumerable
     def my_all?
-        arr = to_a
-            arr.my_each do |item|
-           if yield(item)== true
+        my_each do |object|
+            if yield(object) == true
             return true
            else
             return false
@@ -38,3 +37,35 @@ module Enumerable
         end
     end
 end
+
+
+
+# module Enumerable
+#     def my_any?
+#         arr = to_a
+#         arr.my_each do |i|
+#             if yield(i) == true
+#                 return true
+#             else
+#                 return false
+#             end
+#         end
+#     end
+# end
+
+# enu1 = [10, 12, 18] 
+# res1 = enu1.my_any? { |num| num>13}  
+# puts res1 
+
+module Enumerable
+    def my_none?
+       my_each do |object|
+        if yield(object) == false
+            return true
+        else 
+            return false
+        end
+    end
+end
+end
+
