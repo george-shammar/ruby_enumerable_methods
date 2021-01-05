35b9eae39a28083b8fb252ffa05102083b3ed922
George <george.gbenle@outlook.com>
2021-01-05 10:35:45 -0500

    Creat my_map method

--- enumerables.rb
+++ enumerables.rb
@@ -69,3 +69,43 @@ module Enumerable
 end
 end
 
+# module Enumerable
+#     def my_count
+#         number = 0
+#         length.times do
+#             number +=1
+#             if block_given?
+#                 my_each do |item|
+#                     if yield(item) == true
+#                     end
+#                 end
+#             end
+#         end
+#     end
+# end
+
+#     array = [3, 7, 5, 9, 43, 4, 2, 1, 3, 5, 9, 12, 34, 45, 67, 43, 56, 78]
+
+#     puts array.my_count
+
+#     puts array.my_count { |x| x > 200 }
+
+# module Enumerable
+#     def my_map
+#         arr = to_a
+#         arr.length.times do |object|
+#             fresh = (yield arr[object])
+#             return fresh
+#         end
+#     end
+# end
+
+module Enumerable
+    def my_map(obj = nil)
+        array = []
+        my_each do |i|
+            array.push(yield(i))
+        end
+        array
+      end
+
