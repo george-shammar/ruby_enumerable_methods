5f4e8f4b8910ff29e7343a026530e55f18082f7e
George <george.gbenle@outlook.com>
2021-01-06 11:54:22 -0500

    fis linter errors

--- enumerables.rb
+++ enumerables.rb
@@ -111,8 +111,8 @@ end
 # res3 = enu1.my_none? { |num| num > 4 }
 # puts res3
 
-# my_count
-puts [2, 4, 5, 2, 2].my_count { |i| i == 2 }
+# # my_count
+# puts [2, 4, 5, 2, 2].my_count { |i| i == 2 }
 
 # # my_map
 # test = [2, 4, 5]
