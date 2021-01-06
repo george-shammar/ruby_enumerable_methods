4b512666e968c04bd3ca79a107ea89ab123b0d2e
George <george.gbenle@outlook.com>
2021-01-06 11:41:04 -0500

    Correct linters error

--- enumerables.rb
+++ enumerables.rb
@@ -84,43 +84,43 @@ module Enumerable
   end
 end
 
-# Testing the enumerable methods.
-# my_each
-[1, 2, 3, 4, 5].my_each do |i|
-  puts i * 5
-end
+# # Testing the enumerable methods.
+# # my_each
+# [1, 2, 3, 4, 5].my_each do |i|
+#   puts i * 5
+# end
 
-# my_each_with_index
-top = ['Walmart', 'Exxon Mobil', 'Apple']
-top.my_each_with_index do |company, index|
-  puts "#{index}. #{company}"
-end
+# # my_each_with_index
+# top = ['Walmart', 'Exxon Mobil', 'Apple']
+# top.my_each_with_index do |company, index|
+#   puts "#{index}. #{company}"
+# end
 
-# my_select
-arry = [1, 2, 3, 4, 5, 6]
-puts arry.my_select { |n| n.even? }
+# # my_select
+# arry = [1, 2, 3, 4, 5, 6]
+# puts arry.my_select { |n| n.even? }
 
-# my_all?
-enu1 = [10, 19, 18]
-res1 = enu1.my_all? { |num| num > 13 }
-puts res1
+# # my_all?
+# enu1 = [10, 19, 18]
+# res1 = enu1.my_all? { |num| num > 13 }
+# puts res1
 
-# my_any?
-res2 = enu1.my_any? { |num| num > 13 }
-puts res2
+# # my_any?
+# res2 = enu1.my_any? { |num| num > 13 }
+# puts res2
 
-# my_none?
-res3 = enu1.my_none? { |num| num > 4 }
-puts res3
+# # my_none?
+# res3 = enu1.my_none? { |num| num > 4 }
+# puts res3
 
-# my_count
-puts [2, 4, 5, 2, 2].my_count { |i| i == 2 }
+# # my_count
+# puts [2, 4, 5, 2, 2].my_count { |i| i == 2 }
 
 # my_map
 puts [2, 4, 5].my_map { |i|
-    i += 10
+    i * 10
 }
 
-my_proc = Proc.new { |x| x + 7 }
-arr = [4, 16, 9]
-puts arr.my_map(&my_proc)
+# my_proc = proc { |x| x + 7 }
+# arr = [4, 16, 9]
+# puts arr.my_map(&my_proc)
