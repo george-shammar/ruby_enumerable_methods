1fd1310d67ee66b9ddecf94a50b2cd0aed87e0b9
George <george.gbenle@outlook.com>
2021-01-06 12:29:26 -0500

    Fix indentation error

--- enumerables.rb
+++ enumerables.rb
@@ -23,7 +23,7 @@ module Enumerable
   def my_all?
     my_each do |object|
       return true if yield(object) == true
-      
+
       return false
     end
   end
@@ -73,44 +73,47 @@ module Enumerable
   end
 end
 
-# Testing the enumerable methods.
-# my_each
-[1, 2, 3, 4, 5].my_each do |i|
-  puts i * 5
-end
+# # Testing the enumerable methods.
+# # my_each
+# [1, 2, 3, 4, 5].my_each do |i|
+#   puts i * 5
+# end
 
-# my_each_with_index
-top = ['Walmart', 'Exxon Mobil', 'Apple']
-top.my_each_with_index do |company, index|
-  puts "#{index}. #{company}"
-end
+# # my_each_with_index
+# top = ['Walmart', 'Exxon Mobil', 'Apple']
+# top.my_each_with_index do |company, index|
+#   puts "#{index}. #{company}"
+# end
 
 # my_select
-arry = [1, 2, 3, 4, 5, 6]
-puts arry.my_select { |n| n.even? }
+[2, 4, 5].my_select { |i|
+    if i % 2 == 0
+        puts i
+    end
+}
 
-# my_all?
-enu1 = [10, 19, 18]
-res1 = enu1.my_all? { |num| num > 12 }
-puts res1
+# # my_all?
+# enu1 = [10, 19, 18]
+# res1 = enu1.my_all? { |num| num > 12 }
+# puts res1
 
-# my_any?
-res2 = enu1.my_any? { |num| num > 23 }
-puts res2
+# # my_any?
+# res2 = enu1.my_any? { |num| num > 23 }
+# puts res2
 
-# my_none?
-res3 = enu1.my_none? { |num| num > 4 }
-puts res3
+# # my_none?
+# res3 = enu1.my_none? { |num| num > 4 }
+# puts res3
 
-# my_count
-puts [2, 4, 5, 2, 2].my_count { |i| i == 2 }
+# # my_count
+# puts [2, 4, 5, 2, 2].my_count { |i| i == 2 }
 
-# my_map
-test = [2, 4, 5]
-test.my_map do |i|
-  puts i * 10
-end
+# # my_map
+# test = [2, 4, 5]
+# test.my_map do |i|
+#   puts i * 10
+# end
 
-my_proc = proc { |x| x + 7 }
-arr = [4, 16, 9]
-puts arr.my_map(&my_proc)
+# my_proc = proc { |x| x + 7 }
+# arr = [4, 16, 9]
+# puts arr.my_map(&my_proc)
