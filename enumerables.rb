a1b4aa12dc818b870a0630cd48ab01d9882117ad
George <george.gbenle@outlook.com>
2021-01-06 12:20:26 -0500

    Fix linter errors

--- enumerables.rb
+++ enumerables.rb
@@ -23,7 +23,8 @@ module Enumerable
   def my_all?
     my_each do |object|
       return true if yield(object) == true
-        return false
+      
+      return false
     end
   end
 
@@ -31,22 +32,23 @@ module Enumerable
     my_each do |i|
       return true if yield(i) == true
     end
-      false
+    false
   end
 
   def my_none?
     my_each do |object|
       return true if yield(object) == false
-        return false
+
+      return false
     end
   end
 
   def my_count
     number = 0
-      my_each do |item|
-        number += 1 if yield(item) == true
-      end
-      number
+    my_each do |item|
+      number += 1 if yield(item) == true
+    end
+    number
   end
 
   def my_map(*)
@@ -71,44 +73,44 @@ module Enumerable
   end
 end
 
-# # Testing the enumerable methods.
-# # my_each
-# [1, 2, 3, 4, 5].my_each do |i|
-#   puts i * 5
-# end
+# Testing the enumerable methods.
+# my_each
+[1, 2, 3, 4, 5].my_each do |i|
+  puts i * 5
+end
 
-# # my_each_with_index
-# top = ['Walmart', 'Exxon Mobil', 'Apple']
-# top.my_each_with_index do |company, index|
-#   puts "#{index}. #{company}"
-# end
+# my_each_with_index
+top = ['Walmart', 'Exxon Mobil', 'Apple']
+top.my_each_with_index do |company, index|
+  puts "#{index}. #{company}"
+end
 
-# # my_select
-# arry = [1, 2, 3, 4, 5, 6]
-# puts arry.my_select { |n| n.even? }
+# my_select
+arry = [1, 2, 3, 4, 5, 6]
+puts arry.my_select { |n| n.even? }
 
 # my_all?
 enu1 = [10, 19, 18]
 res1 = enu1.my_all? { |num| num > 12 }
 puts res1
 
-# # my_any?
-# res2 = enu1.my_any? { |num| num > 23 }
-# puts res2
+# my_any?
+res2 = enu1.my_any? { |num| num > 23 }
+puts res2
 
-# # my_none?
-# res3 = enu1.my_none? { |num| num > 4 }
-# puts res3
+# my_none?
+res3 = enu1.my_none? { |num| num > 4 }
+puts res3
 
-# # my_count
-# puts [2, 4, 5, 2, 2].my_count { |i| i == 2 }
+# my_count
+puts [2, 4, 5, 2, 2].my_count { |i| i == 2 }
 
-# # my_map
-# test = [2, 4, 5]
-# test.my_map do |i|
-#   puts i * 10
-# end
+# my_map
+test = [2, 4, 5]
+test.my_map do |i|
+  puts i * 10
+end
 
-# my_proc = proc { |x| x + 7 }
-# arr = [4, 16, 9]
-# puts arr.my_map(&my_proc)
+my_proc = proc { |x| x + 7 }
+arr = [4, 16, 9]
+puts arr.my_map(&my_proc)
